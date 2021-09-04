66d258f93b97f6f48aa953b083ef864ed3ba21d2
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2021-09-04 14:40:27 +0800

    refactor($app): specify minSdk 26

--- app/src/main/java/com/github/johnnymillergh/android/androidjetpackmvvmboilerplate/MainActivity.kt
+++ app/src/main/java/com/github/johnnymillergh/android/androidjetpackmvvmboilerplate/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.github.johnnymillergh.android.androidjetpackmvvmboilerplate
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+import java.time.LocalDateTime
 
 /**
  * MainActivity
@@ -10,9 +12,10 @@ import androidx.appcompat.app.AppCompatActivity
  *
  * @author Johnny Miller (锺俊), email: johnnysviva@outlook.com, 9/4/21: 12:15 PM
  **/
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onStart() {
+        super.onStart()
+        Timber.i("onStart() was called at %s", LocalDateTime.now())
     }
 }
